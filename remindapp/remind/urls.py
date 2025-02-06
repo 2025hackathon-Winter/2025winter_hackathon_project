cbb0a0ae2601b8f1891b30e6106bdc93f329b738
uchiyama <fireground1296@gmail.com>
2025-02-06 14:52:22 +0900

    login_logout_add

--- remindapp/remind/urls.py
+++ remindapp/remind/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from .views import HelloWorldClass
+from .views import CustomLoginView
+from .views import CustomLogoutView
+from . import views # 2025/1/31　うっちゃん追加
+from django.contrib.auth import views as auth_views # 2025/1/31　うっちゃん追加
+
 
 urlpatterns = [
+    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/',CustomLogoutView.as_view(),name='logout'),
+    path('menu/', views.menuview, name='menu'),
+    path('', views.index, name='index'), # 2025/1/31　うっちゃん追加
     path('admin/', admin.site.urls),
     path('hello/', HelloWorldClass.as_view())
 ]
--- remindapp/remind/views.py
+++ remindapp/remind/views.py
@@ -2,10 +2,36 @@ from django.shortcuts import render
 from pathlib import Path
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
 
 def helloworldfunction(reqest):
     returnedobject = HttpResponse('<h1>hello world</h1>')
     return returnedobject
 
+# 2025/2/1 うっちゃん追加　ログイン成功後ルーティング
+def index(request):    # ブラウザからアクセスがあった時の処理
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    
+    # def form_valid(self, form):
+    #     user = form.get_user()
+    #     print(user)
+
+    #     # 入力値がDBにいるか検証
+    next_page = 'logout'
+
+class CustomLogoutView(LogoutView):
+    next_page = '/login/'
+
+    
+def menuview(request):
+    return render(request,'login2.html')
+    
+
 class HelloWorldClass(TemplateView):
     template_name = 'base.html'
+
