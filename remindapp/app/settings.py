7990836bb6a72eaed9ae0be2e603229c13f22aab
anazawa <anazawa.71114@gmail.com>
2025-02-09 23:08:45 +0900

    docker-compose.ymlの設定変更・CSS追加

--- remindapp/app/settings.py
+++ remindapp/app/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +113,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "remindapp", "static"),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
