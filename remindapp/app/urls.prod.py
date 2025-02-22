587ab98853c88e338a5507a9a9bdef6f40fbf6f4
nyori <nyorikokestasaqui@gmail.com>
2025-02-23 00:38:32 +0900

    [FIX]product環境用のファイル追加

---
+++ remindapp/app/urls.prod.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('remind/', include('remind.urls')), 
+    path('', include('remind.urls')),  # remind アプリのURLを統合
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
