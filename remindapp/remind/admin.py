378f34267b4dc830fb18adf15dd020b1dae3b6b5
uchiyama <fireground1296@gmail.com>
2025-02-24 18:09:29 +0900

    [add]メニュー画面の表示/買った・延長の実装/新規物品登録など

--- remindapp/remind/admin.py
+++ remindapp/remind/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 
 class CustomUsersAdmin(UserAdmin):
     model = CustomUsers
-    list_display = ("id", "mailaddress", "default_term", "is_staff", "is_active")
+    list_display = ("uuid", "mailaddress", "default_term", "is_staff", "is_active")
     fieldsets = (
         (None, {"fields": ("mailaddress", "password")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
